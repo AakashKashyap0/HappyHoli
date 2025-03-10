e1973703ef096cbf65ff9f677d2d6d68c794ba9a
Aakash Kashyap <aakashraikwar110@gmail.com>
2025-03-10 10:19:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function playMusic() {
+    let bgMusic = new Audio('music.mp3');
+    bgMusic.loop = true;
+    bgMusic.play();
+}
+
+function startFirework() {
+    let fireworkSound = new Audio('firework.mp3');
+    fireworkSound.loop = true;
+    fireworkSound.play();
+}
